0c9c29358cc44bff039dfb356c6263b2df1eb2a6
MEH4231 <105253042+MEH4231@users.noreply.github.com>
2023-11-10 14:57:52 +1100

    Simple Logic
    Added Idle RPM
    Added Engine off RPM decline

--- Engine.gd
+++ Engine.gd
@@ -1,18 +1,42 @@
 extends Node
 
+var EngineOn = false
 
+var Speed = 0
+var RPM = 0
+var GearMaxRPM1 = 3500
+var GearMaxRPM2 = 5800
+var GearMaxRPM3 = 7200
+var GearMaxRPM4 = 10000
 
-var RPM
-var GearMaxRPM1
-var GearMaxRPM2
-var GearMaxRPM3
-var GearMaxRPM4
+func _physics_process(_delta):
+	print(RPM)
+	Controls()
+	if EngineOn == true:
+		RunEngine()
+	else:
+		EngineOff()
 
+func Controls():
+	if Input.is_action_just_pressed("EngineToggle"):
+		EngineOn = !EngineOn
 
+func RunEngine():
+	if Input.is_action_pressed("Accelerate"):
+		RPM += 500
+	elif Input.is_action_pressed("Brake"):
+		pass
+	else:
+		if RPM <= 500:
+			RPM += (RPM / 10) + 1
+		elif RPM > 500:
+			RPM -= (RPM / 10)
 
-
-
-
+func EngineOff():
+	if RPM > 10:
+		RPM -= (RPM / 10)
+	else:
+		RPM = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -21,7 +45,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 
