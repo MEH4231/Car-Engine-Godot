3d395b1ecc4655a52e297b16951c7dcb3e4874d7
MEH4231 <105253042+MEH4231@users.noreply.github.com>
2023-11-10 14:06:02 +1100

    Initial commit

---
+++ Engine.gd
@@ -0,0 +1,27 @@
+extends Node
+
+
+
+var RPM
+var GearMaxRPM1
+var GearMaxRPM2
+var GearMaxRPM3
+var GearMaxRPM4
+
+
+
+
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
